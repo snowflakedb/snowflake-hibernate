3e6defa6ff818b4c6d13ca7ff020368dfcc7d237
Michał Hofman <michal.hofman@snowflake.com>
2024-07-26 19:44:35 +0200

    SNOW-1437295 ensure unrecommended JDBC driver must be forced

---
+++ snowflake-hibernate/src/main/java/net/snowflake/hibernate/dialect/JdbcDriverVersionException.java
@@ -0,0 +1,7 @@
+package net.snowflake.hibernate.dialect;
+
+class JdbcDriverVersionException extends RuntimeException {
+    public JdbcDriverVersionException(Version actual, Version minimalRecommended) {
+        super(String.format("Using driver in version %s must be forced - recommended driver version should be at least %s", actual, minimalRecommended));
+    }
+}
