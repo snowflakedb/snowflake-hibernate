f88dbf5eab3fbe4020babc3296ed55fa45386c2d
Piotr Bulawa <piotr.bulawa@snowflake.com>
2024-10-17 13:45:29 +0200

    Add javadoc

--- snowflake-hibernate/src/main/java/net/snowflake/hibernate/dialect/SnowflakeDialect.java
+++ snowflake-hibernate/src/main/java/net/snowflake/hibernate/dialect/SnowflakeDialect.java
@@ -19,6 +19,9 @@ import org.hibernate.sql.ast.spi.StandardSqlAstTranslatorFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Snowflake SQL Dialect.
+ */
 public class SnowflakeDialect extends Dialect {
   private static final Logger log = LoggerFactory.getLogger(SnowflakeDialect.class);
 
@@ -39,6 +42,10 @@ public class SnowflakeDialect extends Dialect {
    */
   private final Version MINIMAL_DRIVER_VERSION = Version.from("3.13.31");
 
+  /**
+   * Constructor used to instantiate and configure the Dialect behavior.
+   * @param info used to configure the behavior of the Dialect.
+   */
   public SnowflakeDialect(DialectResolutionInfo info) {
     super(info);
     log.trace("Called SnowflakeDialect(DialectResolutionInfo)");
@@ -231,6 +238,12 @@ public class SnowflakeDialect extends Dialect {
     registerKeyword("gscluster");
   }
 
+  /**
+   * Default timestamp precision as in
+   * <a href="https://docs.snowflake.com/en/sql-reference/data-types-datetime#timestamp">Snowflake documentation</a>
+   *
+   * @return the default precision
+   */
   @Override
   public int getDefaultTimestampPrecision() {
     // Default timestamp precision as in Snowflake documentation
@@ -291,6 +304,11 @@ public class SnowflakeDialect extends Dialect {
     return "ILIKE";
   }
 
+  /**
+   * Get the configured {@link TableType} for current instance of the Dialect.
+   *
+   * @return the {@link TableType}
+   */
   public TableType getTableType() {
     return tableType;
   }
